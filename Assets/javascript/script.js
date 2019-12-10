1813f92dce665fb6385b5a7c3dcec431a08e4a8b
unknown <53538316+grlucy@users.noreply.github.com>
2019-12-10 15:00:52 -0500

    timeColor function passes testing

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -33,29 +33,47 @@ $(document).ready(function() {
     $("#date").text(currentDate);
     // Display current time
     $("#time").text(currentTime);
+    // Set colors based on current time
+    timeColor();
     // Update current date and time every second
     let currentTimer = setInterval(function() {
       currentDate = moment().format("MMMM Do, YYYY");
       $("#date").text(currentDate);
       currentTime = moment().format("hh:mm:ss A");
       $("#time").text(currentTime);
-      // Update textarea color for past, present, and future hours
       timeColor();
     }, 1000);
   }
 
+  // Update textarea color for past, present, and future hours
   function timeColor() {
     currentHour = moment().format("hhA");
-    // If currentHour is between 9 and 5, set the id that matches currentHour to background-color:#b7d1e2.
-    if (possibleHours.business.indexOf(currentHour) !== -1) {
-      $("#" + currentHour).css("background-color", "#b7d1e2");
-    }
-    // Set all hours before currentHour to background-color:#e9ecef
-    // Set all hours after currentHour to background-color:#fff
     // If currentHour is before 9, set all hours to background-color:#fff
+    if (possibleHours.before.indexOf(currentHour) !== -1) {
+      $(".hourNotes").css("background-color", "#fff");
+    }
     // If currentHour is after 5, set all hours to background-color:#e9ecef
+    if (possibleHours.after.indexOf(currentHour) !== -1) {
+      $(".hourNotes").css("background-color", "#e9ecef");
+    }
+    // If currentHour is between 9 and 5...
+    if (possibleHours.business.indexOf(currentHour) !== -1) {
+      // Set the id that matches currentHour to background-color: #b7d1e2
+      $("#" + currentHour).css("background-color", "#b7d1e2");
+      // Set all hours before currentHour to background-color:#e9ecef
+      for (let i = 0; i < possibleHours.business.indexOf(currentHour); i++) {
+        $("#" + possibleHours.business[i]).css("background-color", "#e9ecef");
+      }
+      // Set all hours after currentHour to background-color:#fff
+      for (
+        let i = possibleHours.business.length - 1;
+        i > possibleHours.business.indexOf(currentHour);
+        i--
+      ) {
+        $("#" + possibleHours.business[i]).css("background-color", "#fff");
+      }
+    }
   }
 
   init();
-  timeColor();
 });
